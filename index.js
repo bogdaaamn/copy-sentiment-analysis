6d9745bd1edac2fb604279c7398f5ce7ec47e70e
Bogdan Covrig <bogcov@gmail.com>
2020-08-16 17:07:43 +0200

    Test octokit

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
 
+const token = core.getInput('gh_token');
+
+const octo = new github.GitHub(token);
+const repo = github.context.repo;
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+  try {    
+    const heads = await octo.git.listRefs({ ...repo, namespace: 'heads/' });
+    core.info(heads);
   } catch (error) {
     core.setFailed(error.message);
   }
